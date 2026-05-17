fccd89e96100b208a97ab1f6092440a822d686d0
daniela150803 <est.maria.cascavita@unimilitar.edu.co>
2026-05-17 17:12:36 -0500

    correcciones

--- INICIAR.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Instalando dependencias (solo la primera vez)...
-call npm install
-echo.
-echo Iniciando dashboard...
-echo Cuando veas la URL, abre http://localhost:3000 en tu navegador.
-echo.
-node server.js
-pause
